1bbd6456c45f4129dd76d77fd31f269bf7c458fb
TheaHansen <theanoehr@outlook.com>
2024-11-14 12:59:12 +0100

    Created get fetch function

---
+++ src/services/requests.ts
@@ -0,0 +1,14 @@
+const apiUrl = process.env.REACT_APP_API_URL;
+
+function getRequest(endpoint: string): void {
+  if (!apiUrl) {
+    console.log("API URL is not defined");
+    return;
+  }
+  fetch(`${apiUrl}${endpoint}`)
+    .then((response) => response.json())
+    .then((res) => console.log(res))
+    .catch((error) => console.error("Error fetching data:", error));
+}
+
+export { getRequest };
