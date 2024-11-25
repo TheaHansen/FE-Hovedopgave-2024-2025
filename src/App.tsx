07f62f772d37d6e71b1b3441fc2589d9066b63fe
FrederikkeEGTC <frederikkethrane@gmail.com>
2024-11-25 09:46:29 +0100

    navbar import fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import "./App.css";
 import { getRequest } from "./services/requests";
-import NavigationBar from "./components/Navbar";
+import NavigationBar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Topbar from "./components/Topbar";
 import LogoHeader from "./components/LogoHeader";
