aa18e213566f18dabf8580e743cda3ee3f81733b
TheaHansen <theanoehr@outlook.com>
2024-12-03 10:58:35 +0100

    Added button and price to carousel and changed colours

--- src/components/CustomCarousel.tsx
+++ src/components/CustomCarousel.tsx
@@ -1,8 +1,9 @@
 import Carousel from "react-bootstrap/Carousel";
-import image from "../images/IMG_1618.jpg";
 import { useEffect, useState } from "react";
 import Product from "./product/Product.interfaces";
 import { getRequest } from "../services/requests";
+import ToPageButton from "./ToPageButton";
+import "../customCSS/carousel.css";
 
 //Made by Thea
 interface CustomCarouselProps {
@@ -21,14 +22,28 @@ function CustomCarousel({ endpoint }: CustomCarouselProps) {
   }, [endpoint]);
 
   return (
-    <div className="p-3 d-flex justify-content-center">
-      <Carousel style={{ width: 800, height: 500 }}>
+    <div className="p-3 d-flex justify-content-center mb-3">
+      <Carousel
+        data-bs-theme="dark"
+        style={{ width: 800, height: 503 }}
+        className="border carousel-border"
+      >
         {products.map((product) => (
           <Carousel.Item>
-            <img src={image} width="800" height="500" />
+            <img
+              src={require(`../images/${product.imageUrl}`)}
+              width="800"
+              height="500"
+              alt="Product on sale"
+            />
             <Carousel.Caption>
               <h3>{product.title}</h3>
               <p>{product.shortDescription}</p>
+              <p>{product.price} DKK</p>
+              <ToPageButton
+                children="Køb her!"
+                to={`/products/${product.id}`}
+              />
             </Carousel.Caption>
           </Carousel.Item>
         ))}
