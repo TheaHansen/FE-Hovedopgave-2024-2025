43e26a541e2d8eb2c3f34aa00c8d62c6c08094d3
TheaHansen <theanoehr@outlook.com>
2024-12-05 13:26:28 +0100

    Created tests

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,23 +1,29 @@
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
-import "../customCSS/breadcrumb.css"
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import "../customCSS/breadcrumb.css";
 
 interface BreadcrumbProps {
-    items: string[];
+  items: string[];
 }
 
 function CustomBreadcrumb({ items }: BreadcrumbProps) {
-    return (
-        <Breadcrumb className="black">
-          {items.map((item, index) => (
-            <Breadcrumb.Item 
-            key={index} 
-            active={index === items.length - 1}
-            className={index === items.length - 1 ? "active-breadcrumb" : "clickable-breadcrumb"}>
-              {item}
-            </Breadcrumb.Item>
-          ))}
-        </Breadcrumb>
-      );
-};
+  return (
+    <Breadcrumb className="black">
+      {items.map((item, index) => (
+        <Breadcrumb.Item
+          key={index}
+          active={index === items.length - 1}
+          className={
+            index === items.length - 1
+              ? "active-breadcrumb"
+              : "clickable-breadcrumb"
+          }
+          data-testid="breadcrumb-item"
+        >
+          {item}
+        </Breadcrumb.Item>
+      ))}
+    </Breadcrumb>
+  );
+}
 
 export default CustomBreadcrumb;
