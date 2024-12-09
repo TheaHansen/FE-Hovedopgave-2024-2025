1726e8fc0b3e9fa5c0444389a8e02b8bc561f393
TheaHansen <theanoehr@outlook.com>
2024-12-09 12:33:06 +0100

    Added functionality to Add to Basket button

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -6,64 +6,77 @@ import Product from "../components/product/Product.interfaces";
 import { useEffect, useState } from "react";
 import StockStatus from "../components/stockStatus/StockStatus";
 import QuantityPicker from "../components/QuantityPicker";
+import { useBasket } from "../context/BasketContext";
 
 interface SingleProductProps {
-    endpoint: string;
-    breadcrumbItems: string[];
+  endpoint: string;
+  breadcrumbItems: string[];
 }
 
 function SingleProduct({ endpoint, breadcrumbItems }: SingleProductProps) {
-    const { id } = useParams<{ id: string }>();
-    const [product, setProduct] = useState<Product | null>(null);
+  const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [quantity, setQuantity] = useState<number>(1);
 
-    useEffect(() => {
-        if (id) {
-            const productId = parseInt(id, 10);
-            if (isNaN(productId)) {
-                console.error("Invalid product ID");
-                return;
-            }
+  const { addToBasket } = useBasket();
 
-            getRequest(`products/${productId}`)
-                .then((data) => setProduct(data))
-                .catch((error) => console.error("Failed to fetch product: ", error));
-        } else {
-            console.error("Product ID is missing");
-        }
-    }, [id]);
+  useEffect(() => {
+    if (id) {
+      const productId = parseInt(id, 10);
+      if (isNaN(productId)) {
+        console.error("Invalid product ID");
+        return;
+      }
 
-    
-    if (!product) {
-        return <p>Loading product details...</p>;
+      getRequest(`products/${productId}`)
+        .then((data) => setProduct(data))
+        .catch((error) => console.error("Failed to fetch product: ", error));
+    } else {
+      console.error("Product ID is missing");
     }
-    
-    return (
+  }, [id]);
 
-        <div className="container mt-4 mb-4">
-            <div className="row g-5">
-                <div className="col-md-6 pr-md-5">
-                    <img
-                        src={require(`../images/${product.imageUrl}`)}
-                        alt={product.title}
-                        className="img-fluid"
-                    />
-                </div>
-                <div className="col-md-6">
-                    <CustomBreadcrumb items={breadcrumbItems} />
-                    <h1>{product.title}</h1>
-                    <h6 className="mb-2 text-muted small">{product.shortDescription}</h6>
-                    <h6 className="small mb-4">{product.description}</h6>
-                    <h6>{product.price.toFixed(2)} DKK</h6>
-                    <h6 className="d-flex align-items-center mt-2 small">Lagerstatus: <StockStatus stockStatusFromProduct={product.stockStatus} /> </h6>
-                    <div className="d-flex align-items-center mt-4 gap-3">
-                        <QuantityPicker />
-                        <Button variant="dark">Tilføj til kurv</Button>
-                    </div>
-                </div>
-            </div>
+  if (!product) {
+    return <p>Loading product details...</p>;
+  }
+
+  return (
+    <div className="container mt-4 mb-4">
+      <div className="row g-5">
+        <div className="col-md-6 pr-md-5">
+          <img
+            src={require(`../images/${product.imageUrl}`)}
+            alt={product.title}
+            className="img-fluid"
+          />
         </div>
-        
-    );
+        <div className="col-md-6">
+          <CustomBreadcrumb items={breadcrumbItems} />
+          <h1>{product.title}</h1>
+          <h6 className="mb-2 text-muted small">{product.shortDescription}</h6>
+          <h6 className="small mb-4">{product.description}</h6>
+          <h6>{product.price.toFixed(2)} DKK</h6>
+          <h6 className="d-flex align-items-center mt-2 small">
+            Lagerstatus:{" "}
+            <StockStatus stockStatusFromProduct={product.stockStatus} />{" "}
+          </h6>
+          <div className="d-flex align-items-center mt-4 gap-3">
+            <QuantityPicker quantity={quantity} setQuantity={setQuantity} />
+            <Button
+              style={{ zIndex: 1000 }}
+              variant={
+                product.stockStatus === "NotInStock" ? "secondary" : "dark"
+              }
+              onClick={() => addToBasket(product.id, quantity)}
+              disabled={product.stockStatus === "OutOfStock"}
+            >
+              Tilføj til kurv
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SingleProduct;
