aeb849afe3251633cc78db009c2b48b48a46994a
TheaHansen <theanoehr@outlook.com>
2024-12-11 13:58:18 +0100

    Added plugins and server  to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig, UserConfigExport } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
   test: {
     globals: true,
     setupFiles: "src/setupTests.ts",
