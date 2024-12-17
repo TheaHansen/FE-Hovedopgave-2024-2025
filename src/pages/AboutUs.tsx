02ffb436f31c56986ccb1fa1a7a73f17280712ff
TheaHansen <theanoehr@outlook.com>
2024-12-17 09:17:41 +0100

    Added Breadcrumbs and headline to about us page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,5 +1,18 @@
-function AboutUs() {
-  return <></>;
+import CustomBreadcrumb from "../components/Breadcrumb";
+
+interface AboutUsProps {
+  breadcrumbItems?: string[];
+}
+
+function AboutUs({ breadcrumbItems }: AboutUsProps) {
+  return (
+    <div className="container mt-4">
+      {breadcrumbItems && <CustomBreadcrumb items={breadcrumbItems} />}
+      <h1 className="mb-4" data-testid="headline">
+        Om os
+      </h1>
+    </div>
+  );
 }
 
 export default AboutUs;
